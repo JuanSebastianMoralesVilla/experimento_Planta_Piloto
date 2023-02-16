8b67afce756edee2314c3d623304d01b8a7bc9a2
Sebastian Morales <1193029087@u.icesi.edu.co>
2023-02-16 16:03:53 -0500

    upload metodos of csv

---
+++ Code/Client/src/main/java/icesi/plantapiloto/experimento/client_manager/experimento_Planta_Piloto/Tag.java
@@ -0,0 +1,54 @@
+package experimento_Planta_Piloto;
+
+import java.security.Timestamp;
+
+public class Tag {
+	 String name;
+	Double value;
+	Timestamp timeTag;
+	
+	
+	public Tag (String name, Double value, Timestamp timeTag) {
+		
+		this.name=name;
+		this.value=value;
+		this.timeTag=timeTag;
+		
+		
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Double getValue() {
+		return value;
+	}
+
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+
+	public Timestamp getTimeTag() {
+		return timeTag;
+	}
+
+
+	public void setTimeTag(Timestamp timeTag) {
+		this.timeTag = timeTag;
+	}
+	
+	
+	
+	
+
+}
